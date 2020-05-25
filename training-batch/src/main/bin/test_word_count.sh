cb3bcfd362f26fe3ed5c82de6258afbbe46f9ea1
kevinnhu <kevin_789@126.com>
2020-05-25 22:16:03 +0800

    faet: update

--- training-batch/src/main/bin/test_word_count.sh
+++ training-batch/src/main/bin/test_word_count.sh
@@ -3,18 +3,17 @@
 HOME_PATH=$(cd "$(dirname "$0")"; cd ..; pwd)
 
 CMD=$(cat << EOF
-/usr/local/service/flink/binf/flink run \
-  --class com.winfred.training.wordcount.WordCount \
+/usr/local/service/flink/bin/flink run \
+  --class com.winfred.training.wc.WordCount \
   --jobmanager yarn-cluster \
   --yarncontainer 3 \
   --yarnslots 4 \
   --yarnjobManagerMemory 2048 \
   --yarntaskManagerMemory 2048 \
   --parallelism 12 \
-  --yarnstreaming \
   --detached  \
   --yarnname wordcount-test \
-  "${HOME_PATH}/lib/training-batch.jar"
+  ${HOME_PATH}/lib/training-batch.jar
 EOF
 )
 
