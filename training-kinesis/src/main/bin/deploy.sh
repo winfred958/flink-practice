934372072ea4c641add9f1031a423e517be43496
kevin_789 <kevin_789@126.com>
2020-10-19 14:34:50 +0800

    feat: update

--- training-kinesis/src/main/bin/deploy.sh
+++ training-kinesis/src/main/bin/deploy.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
-HOME_PATH=$(cd "$(dirname "$0")"; cd ..; pwd)
+HOME_PATH=$(
+  cd "$(dirname "$0")"
+  cd ..
+  pwd
+)
 
 TAG=${1}
 
-if [[ "x${TAG}" = "x" ]]; then
-   echo -e "需要按tag发布, 请确认tag编号"
-   exit -1
+if [[ "x${TAG}" == "x" ]]; then
+  echo -e "需要按tag发布, 请确认tag编号"
+  exit -1
 else
-    echo -e "tag=${TAG}\n"
+  echo -e "tag=${TAG}\n"
 fi
 
 START_TIME=$(date +%s)
@@ -32,7 +36,8 @@ git log -3
 # 开始编译打包
 MODEL_NAME=tranning-kinesis
 
-CMD=$(cat << EOF
+CMD=$(
+  cat <<EOF
 mvn package --projects ${MODEL_NAME} --also-make --also-make-dependents
 EOF
 )
@@ -48,4 +53,4 @@ rm -rf ${TMP_DIR_PATH}
 END_TIME=$(date +%s)
 
 echo -e "耗时: $(expr ${END_TIME} - ${START_TIME})s"
-echo -e "耗时: $((${END_TIME}-${START_TIME}))s"
+echo -e "耗时: $((${END_TIME} - ${START_TIME}))s"
