50fbe84ab623137ca3564a23e27c4d3a26e14336
winfred958 <winfred958@gmail.com>
2021-01-19 01:08:57 +0800

    feat: update

--- training-streaming/src/main/scala/com/winfred/streamming/example/HbaseExample.scala
+++ training-streaming/src/main/scala/com/winfred/streamming/example/HbaseExample.scala
@@ -1,12 +1,23 @@
 package com.winfred.streamming.example
 
+import com.alibaba.fastjson.JSON
+import com.winfred.core.sink.HbaseSink
 import com.winfred.streamming.common.TestDataMockSource
+import com.winfred.streamming.entity.log.EventEntity
 import org.apache.flink.streaming.api.CheckpointingMode
 import org.apache.flink.streaming.api.environment.CheckpointConfig.ExternalizedCheckpointCleanup
 import org.apache.flink.streaming.api.scala.StreamExecutionEnvironment
 import org.apache.hadoop.hbase.client.Put
 
 object HbaseExample {
+
+  private val zookeeperQuorum = ""
+
+  private val tableName = "example"
+
+  private val family = "text"
+  private val qualifier = "str"
+
 
   def main(args: Array[String]): Unit = {
 
@@ -21,9 +32,19 @@ object HbaseExample {
     executionEnvironment
       .addSource(new TestDataMockSource(2, 20))
       .map(str => {
-        val put = new Put("".getBytes())
 
+        val entity = JSON.parseObject(str, classOf[EventEntity])
+        val rowKey = entity.getUuid
+        val put = new Put(rowKey.getBytes())
+          .addColumn(
+            family.getBytes(),
+            qualifier.getBytes(),
+            System.currentTimeMillis(),
+            str.getBytes()
+          )
+        put
       })
+      .addSink(new HbaseSink(zookeeperQuorum, tableName))
 
     executionEnvironment
       .execute("hbase-test")
