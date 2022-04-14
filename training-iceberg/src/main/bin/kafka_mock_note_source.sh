5b2d829a90ec0887a68fb01059ce99be19305f33
kai.hu <winfred958@gmail.com>
2022-04-14 14:30:18 +0800

    feat: udpate shell

--- training-iceberg/src/main/bin/kafka_mock_note_source.sh
+++ training-iceberg/src/main/bin/kafka_mock_note_source.sh
@@ -12,17 +12,17 @@ source ~/.bash_profile
 CMD=$(
   cat <<EOF
 ${FLINK_HOME}/bin/flink run \
-  --class com.winfred.streamming.kafka.NodeMessageMock \
+  --class com.winfred.iceberg.demo.IcebergUpsertDemo \
   --jobmanager yarn-cluster \
   --yarnslots 1 \
   --yarnjobManagerMemory 1024 \
   --yarntaskManagerMemory 1024 \
-  --parallelism 3 \
+  --parallelism 1 \
   --detached \
   --yarnname NodeMessageMock \
   --yarnqueue default \
+  --yarnship ${HOME_PATH}/lib/iceberg-flink-runtime-1.14-0.13.1.jar \
   ${HOME_PATH}/lib/training-streaming.jar \
-  --topic-name kafka_test_raw \
   --interval-min 2 \
   --interval-max 10
 EOF
