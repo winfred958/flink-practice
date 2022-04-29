50d56dd9717fe943cfa210c03ec09900dbe0c69c
kai.hu <winfred958@gmail.com>
2022-04-29 10:04:49 +0800

    feat: update

--- training-core/src/main/java/com/winfred/core/source/NoteMessageMockSource.java
+++ training-core/src/main/java/com/winfred/core/source/NoteMessageMockSource.java
@@ -44,7 +44,7 @@ public class NoteMessageMockSource extends RichParallelSourceFunction<NoteMock> 
         Object lock = ctx.getCheckpointLock();
         while (isRun) {
             synchronized (lock) {
-                final int size = RandomUtils.nextInt(30, 100);
+                final int size = RandomUtils.nextInt(100, 200);
                 mockNoteInfo(size)
                     .forEach(ctx::collect);
             }
@@ -58,23 +58,30 @@ public class NoteMessageMockSource extends RichParallelSourceFunction<NoteMock> 
     }
 
     private List<NoteMock> mockNoteInfo(int size) {
-        List<NoteMock> result = new ArrayList<>(10);
+        List<NoteMock> result = new ArrayList<>(size);
         for (int i = 0; i < size; i++) {
             // mock 短信发送
             final NoteSendRaw send = getSendEntity();
             final String primaryKey = send.getPrimaryKey();
-            final String receiver = send.getReceiver();
+            final LocalDateTime channelSendTime = send.getChannel_send_time();
 
-            // 随机发送重复数据
+            // 随机发送重复数据, 千分之一重复
+            final int sign = RandomUtils.nextInt(0, 1000);
             for (int repeat = 1; repeat <= RandomUtils.nextInt(1, 3); repeat++) {
                 result.add(send);
+                if (sign != 1) {
+                    break;
+                }
             }
 
             // mock 发送短信的回执
-            final NoteReceiptRaw receipt = getReceipt(primaryKey, receiver);
+            final NoteReceiptRaw receipt = getReceipt(primaryKey, channelSendTime);
             // 随机发送重复数据
             for (int repeat = 1; repeat <= RandomUtils.nextInt(1, 3); repeat++) {
                 result.add(receipt);
+                if (sign != 1) {
+                    break;
+                }
             }
             result.add(receipt);
         }
@@ -105,23 +112,28 @@ public class NoteMessageMockSource extends RichParallelSourceFunction<NoteMock> 
         send.setCharge_submit_num(RandomUtils.nextLong(1, 2000));
 
         final Instant instant = Instant.ofEpochMilli(System.currentTimeMillis());
+
+
         final LocalDateTime localDateTime = LocalDateTime.ofInstant(instant, zoneId);
         send.setBusiness_request_time(localDateTime);
-        send.setChannel_send_time(localDateTime);
+
+        final LocalDateTime sendTime = LocalDateTime.now().minusDays(RandomUtils.nextLong(0, 7));
+        send.setChannel_send_time(sendTime);
 
         send.setExt_json(JSON.toJSONString(send));
         return send;
     }
 
-    private NoteReceiptRaw getReceipt(String primaryKey, String receiver) {
+    private NoteReceiptRaw getReceipt(String primaryKey, final LocalDateTime channelSendTime) {
         final NoteReceiptRaw receipt = new NoteReceiptRaw();
         receipt.setPrimary_key(primaryKey);
         receipt.setSp_result(String.valueOf(RandomUtils.nextBoolean()));
         receipt.setSp_charge_submit_num(RandomUtils.nextLong(0, 999L));
-        final LocalDateTime now = LocalDateTime.now(zoneId);
-        receipt.setSp_send_time(now);
-        receipt.setChannel_receive_time(now);
-        receipt.setReceive_system_time(now);
+
+        final LocalDateTime spSendTime = LocalDateTime.from(channelSendTime).plusHours(RandomUtils.nextLong(0, 72));
+        receipt.setSp_send_time(spSendTime);
+        receipt.setChannel_receive_time(spSendTime.plusMinutes(1));
+        receipt.setReceive_system_time(LocalDateTime.now());
 
         return receipt;
     }
