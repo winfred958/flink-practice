38be284e707d1744ca04c4f1cccc09c2e0eaf6af
kai.hu <winfred958@gmail.com>
2022-05-05 13:32:27 +0800

    feat: update

--- training-core/src/main/java/com/winfred/core/source/NoteMessageMockSource.java
+++ training-core/src/main/java/com/winfred/core/source/NoteMessageMockSource.java
@@ -119,9 +119,12 @@ public class NoteMessageMockSource extends RichParallelSourceFunction<NoteMock> 
         send.setBusiness_request_time(localDateTime);
 
         LocalDateTime sendTime = LocalDateTime.now();
-        final int timeRand = RandomUtil.randomInt(0, 10000);
+        final int timeRand = RandomUtil.randomInt(0, 1000);
         if (timeRand < 1) {
-            sendTime = LocalDateTime.now().minusDays(RandomUtils.nextLong(0, 7));
+            sendTime = LocalDateTime.now().minusDays(RandomUtils.nextLong(4, 7));
+        }
+        if (timeRand < 10) {
+            sendTime = LocalDateTime.now().minusDays(RandomUtils.nextLong(0, 4));
         }
 
         send.setChannel_send_time(sendTime);
